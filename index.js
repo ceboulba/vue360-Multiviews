75d63988253ac8561b3f8e93eeb12e7ebee8c7b3
ceboulba <antoine@archipicture.fr>
2019-05-10 14:36:33 +0200

    2 files modified

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 import './style.css'
 import * as BABYLON from 'babylonjs'
 const canvas = document.getElementById('renderCanvas')
-var img = 'http://www.bandoltourisme.fr/fileadmin/Image-Tourisme/actualites/20190328_Bandol_360__.jpg'
+//var img = 'http://www.bandoltourisme.fr/fileadmin/Image-Tourisme/actualites/20190328_Bandol_360__.jpg'
 var imgs = [
   'https://res.cloudinary.com/archipicture/image/upload/v1557355560/ca_pano.jpg',
   'https://res.cloudinary.com/archipicture/image/upload/v1557472339/bandol.jpg',
   'https://res.cloudinary.com/archipicture/image/upload/v1557472339/city.jpg'
 ]
- 
-const btnPrev = document.getElementById('btnPrev')
-const btnNext = document.getElementById('btnNext')
 
 let num = 0
-
+const btnPrev = document.getElementById('btnPrev')
+const btnNext = document.getElementById('btnNext')
+btnNext.onclick = () => {
+  next()
+}
+btnPrev.onclick = () => {
+  prev()
+}
 
 
 var engine = new BABYLON.Engine(canvas, true)
 
-var createScene = function() {
+var createScene = function () {
   var scene = new BABYLON.Scene(engine)
   var camera = new BABYLON.ArcRotateCamera(
     'Camera',
@@ -36,48 +40,44 @@ var createScene = function() {
 
   var dome = new BABYLON.PhotoDome(
     'testdome',
-    img,
+    imgs[num],
     {
       resolution: 32,
-      size:15,
+      size: 15,
       useDirectMapping: false,
     },
     scene
   )
-  btnNext.onclick = function(){
-  next()
-}
-btnPrev.onclick = function(num){
-  prev()
-}
-
-  const next = () => {
-  event.preventDefault()
-   num < imgs.length - 1 ?
-   num++
-   : num = 0
-  console.log('coucou next')
-  console.log(num)
-}
-
-const prev = () => {
-  event.preventDefault()
-  num === 0 ?
-   num = imgs.length - 1
-    : num--
-  console.log('coucou previous')
-  console.log(num)
-   }
 
   return scene
 }
 
 const scene = createScene()
 
-engine.runRenderLoop(function() {
+const next = () => {
+  event.preventDefault()
+  num < imgs.length - 1 ?
+    num++
+    : num = 0
+  console.log('coucou next')
+  console.log(num)
+  scene = createScene()
+}
+
+const prev = () => {
+  event.preventDefault()
+  num === 0 ?
+    num = imgs.length - 1
+    : num--
+  console.log('coucou previous')
+  console.log(num)
+    scene = createScene()
+}
+
+engine.runRenderLoop(function () {
   scene.render()
 })
 
-window.addEventListener('resize', function() {
+window.addEventListener('resize', function () {
   engine.resize()
 })
