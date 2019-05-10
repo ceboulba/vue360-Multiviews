a13bd8e753ea9e0f98286f6ebee7be87d55b5e40
ceboulba <antoine@archipicture.fr>
2019-05-10 21:28:44 +0200

    1 file modified

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ import * as BABYLON from 'babylonjs'
 const canvas = document.getElementById('renderCanvas')
 var imgs = [
   'https://res.cloudinary.com/archipicture/image/upload/v1557511607/sham_milan/sham_milan_vue_01.jpg',
-  'https://res.cloudinary.com/archipicture/image/upload/v1557511607/sham_milan/sham_milan_vue_02.jpg',
-  'https://res.cloudinary.com/archipicture/image/upload/v1557512316/sham_milan/sham_milan_vue_03.jpg',
-  'https://res.cloudinary.com/archipicture/image/upload/v1557512316/sham_milan/sham_milan_vue_04.jpg'
+  'https://res.cloudinary.com/archipicture/image/upload/v1557511871/sham_milan/sham_milan_vue_02.jpg',
+  'https://res.cloudinary.com/archipicture/image/upload/v1557512775/sham_milan/sham_milan_vue_03.jpg',
+  'https://res.cloudinary.com/archipicture/image/upload/v1557514455/sham_milan/sham_milan_vue_04.jpg'
 ]
 let num = 0
 const btnPrev = document.getElementById('btnPrev').addEventListener('click',()=> prev())
