650feaad27a4e60e87fa1eee5b2cade0c1ac7eba
ceboulba <antoine@archipicture.fr>
2019-05-10 20:23:53 +0200

    1 file modified

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const canvas = document.getElementById('renderCanvas')
 var imgs = [
   'https://res.cloudinary.com/archipicture/image/upload/v1557511607/sham_milan/sham_milan_vue_01.jpg',
   'https://res.cloudinary.com/archipicture/image/upload/v1557511607/sham_milan/sham_milan_vue_02.jpg',
-  'https://res.cloudinary.com/archipicture/image/upload/v1557512316/sham_milan/sham_milan_vue_03.jpg'
+  'https://res.cloudinary.com/archipicture/image/upload/v1557512316/sham_milan/sham_milan_vue_03.jpg',
+  'https://res.cloudinary.com/archipicture/image/upload/v1557512316/sham_milan/sham_milan_vue_04.jpg'
 ]
 
 let num = 0
