b03b4bd4703d07f5520a5465ebb34b3b749e695b
ceboulba <antoine@archipicture.fr>
2019-05-11 14:15:51 +0200

    1 file modified

--- index.js
+++ index.js
@@ -54,13 +54,11 @@ const scene = createScene()
 
 const check = () => {
   num === 0 ? (() => {
-    alert('num: ',num)
     canvas.classList.add('hide');
     imgBox.classList.remove('hide');
     })()
 
     : (() => {
-    alert('num: ',num)
       canvas.classList.remove('hide')
       imgBox.classList.add('hide')
     })()
